15b3e75ad4aefdf1b6f3a69e22e716736aa8c314
ringo <c0a24061de@edu.teu.ac.jp>
2025-11-25 14:41:54 +0900

    練習4：衝突

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -50,6 +50,10 @@ def main():
                 return
         screen.blit(bg_img, [0, 0]) 
 
+        if kk_rct.colliderect(bb_rct): #こうかとんと爆弾がぶつかったら
+            print("ゲームオーバー")
+            return
+
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         # if key_lst[pg.K_UP]:
