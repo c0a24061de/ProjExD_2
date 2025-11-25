ed38d7edb509f256e29845f24b85cd5c1651d31e
ringo <c0a24061de@edu.teu.ac.jp>
2025-11-25 16:36:31 +0900

    演習2：加速と巨大化

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -22,31 +22,29 @@ def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
     画面内ならTrue，画面外ならFalse
     """
     yoko, tate = True, True
-    if rct.left < 0 or WIDTH <rct.right:  # 横方向のはみ出しチェック
+    if rct.left < 0 or WIDTH < rct.right:  # 横方向のはみ出しチェック
         yoko = False
     if rct.top < 0 or HEIGHT < rct.bottom:  # 縦方向のはみ出しチェック
         tate = False
     return yoko, tate
 
+
 def gameover(screen: pg.Surface) -> None:
-    
-    # 黒背景の半透明Surface
+    """
+    爆弾が着弾した際に、画面をブラックアウトし泣いているこうかとん画像とgameoverの文字列を
+    5秒間表示させる関数。
+    """
     blackout = pg.Surface((WIDTH, HEIGHT))
     blackout.set_alpha(200)
-    blackout.fill((0, 0, 0))
 
-    # Game Over 文字
     font = pg.font.Font(None, 80)
     text = font.render("Game Over", True, (255, 255, 255))
     text_rct = text.get_rect(center=(WIDTH // 2, HEIGHT // 2))
 
-    # 泣いているこうかとん画像
-    cry_img = pg.transform.rotozoom(pg.image.load("fig/8.png"), 0, 1.0)
+    cry_img = pg.image.load("fig/8.png")
     cry_rct = cry_img.get_rect(center=(WIDTH // 2 - 200, HEIGHT // 2))
     cry_rct2 = cry_img.get_rect(center=(WIDTH // 2 + 200, HEIGHT // 2))
 
-
-    # 描画
     screen.blit(blackout, (0, 0))
     screen.blit(text, text_rct)
     screen.blit(cry_img, cry_rct)
@@ -56,6 +54,19 @@ def gameover(screen: pg.Surface) -> None:
     time.sleep(5)
 
 
+def init_bb_imgs() -> tuple[list[int], list[pg.Surface]]:
+    """
+    サイズの異なる爆弾画像と加速度リストを返す
+    """
+    bb_imgs = []
+    bb_accs = [a for a in range(1, 11)]
+    for r in range(1, 11):
+        bb_img = pg.Surface((20*r, 20*r))
+        pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)
+        bb_img.set_colorkey((0, 0, 0)) 
+        bb_imgs.append(bb_img)
+    return bb_accs, bb_imgs
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -64,15 +75,17 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
-    bb_img = pg.Surface((20, 20))  # 空のSurface
-    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 半径10の赤い円を描画
-    bb_img.set_colorkey((0, 0, 0))  # 黒色を透過色に設定
-    bb_rct = bb_img.get_rect()  # 爆弾Rect
-    bb_rct.centerx = random.randint(0, WIDTH)  # 爆弾横座標
-    bb_rct.centery = random.randint(0, HEIGHT)  # 爆弾縦座標
-    vx, vy = +5, +5  # 爆弾の横速度，縦速度
+    
+    bb_accs, bb_imgs = init_bb_imgs()
+    
+    bb_img = bb_imgs[0]
+    bb_rct = bb_img.get_rect()
+    bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+    vx, vy = +5, +5
+    
     clock = pg.time.Clock()
     tmr = 0
+    
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -83,32 +96,36 @@ def main():
             gameover(screen)
             return
 
-
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
-        # if key_lst[pg.K_UP]:
-        #     sum_mv[1] -= 5
-        # if key_lst[pg.K_DOWN]:
-        #     sum_mv[1] += 5
-        # if key_lst[pg.K_LEFT]:
-        #     sum_mv[0] -= 5
-        # if key_lst[pg.K_RIGHT]:
-        #     sum_mv[0] += 5
         for key, mv in DELTA.items():
             if key_lst[key]:
-                sum_mv[0] += mv[0]  # 横方向の移動量
-                sum_mv[1] += mv[1]  # 縦方向の移動量
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
         kk_rct.move_ip(sum_mv)
-        if check_bound(kk_rct) != (True, True):  # 画面外なら
-            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])  # 移動をなかったことにする
+        
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
+                
+        avx = vx * bb_accs[min(tmr // 500, 9)]
+        avy = vy * bb_accs[min(tmr // 500, 9)]
+
+        bb_img = bb_imgs[min(tmr // 500, 9)]
+
+        bb_rct.width = bb_img.get_width()
+        bb_rct.height = bb_img.get_height()
+        
+        bb_rct.move_ip(avx, avy)
+
         yoko, tate = check_bound(bb_rct)
-        if not yoko:  # 横方向にはみ出ていたら
+        if not yoko:
             vx *= -1
-        if not tate:  # 縦方向にはみ出ていたら
+        if not tate:
             vy *= -1
-        bb_rct.move_ip(vx, vy)
+        
         screen.blit(bb_img, bb_rct)
+
         pg.display.update()
         tmr += 1
         clock.tick(50)
